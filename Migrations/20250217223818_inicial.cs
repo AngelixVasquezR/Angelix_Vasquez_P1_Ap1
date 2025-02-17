cc6a3bd444f9ad1edddbbbd2c732971981dcb32e
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-02-17 18:48:52 -0400

    Migracion y base de datos lista

---
+++ Migrations/20250217223818_inicial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Angelix_Vasquez_P1_Ap1.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Modelo",
+                columns: table => new
+                {
+                    ModeloId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modelo", x => x.ModeloId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Modelo");
+        }
+    }
+}
