1306540dc1e85748ef4018632b2dabf348716e86
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-02-17 15:57:16 -0400

    Creacion de Modelo de  ejemplo

---
+++ Models/Modelo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Angelix_Vasquez_P1_Ap1.Models;
+
+public class Modelo
+{
+    [Key]
+    public int ModeloId { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    public string? Nombre { get; set; }
+
+
+}
