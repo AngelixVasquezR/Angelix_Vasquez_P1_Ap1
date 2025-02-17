e4969cd574fa21d67a1d456315fb315200224194
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-02-17 16:11:06 -0400

    Creacion del contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Angelix_Vasquez_P1_Ap1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Angelix_Vasquez_P1_Ap1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Modelo> Modelo { get; set; }
+}
